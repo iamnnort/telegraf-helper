3a78e528cb71f22b7858d97737db67e34927aa1a
Nikita Pavets <nikitapavets@gmail.com>
2023-10-13 13:39:48 +0000

    add BAD_REQUEST_REMOVE_OWNER

--- src/config.ts
+++ src/config.ts
@@ -4,6 +4,7 @@ export const config = {
       FORBIDDEN_USER_DEACTIVATED: "Forbidden: user is deactivated",
       FORBIDDEN_BOT_BLOCKED: "Forbidden: bot was blocked by the user",
       BAD_REQUEST_REMOVE_OWNER: "Bad Request: can't remove chat owner",
+      BAD_REQUEST_CHAT_NOT_FOUND: "Bad Request: chat not found",
     },
   },
 };
